1e5eb54506749bf5c0f113467c76c9b1ab45c270
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-01-04 10:26:31 +0100

    Initial setup

---
+++ src/day1.rs
@@ -0,0 +1,10 @@
+
+pub const PARTS: [fn(); 2] = [part1, part2];
+
+fn part1() {
+    
+}
+
+fn part2() {
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::time;
+
+mod day1;
+
+fn main() {
+    let funcs = [day1::PARTS];
+    let mut args = env::args();
+    args.next();
+    if let Some(x) = args.next() {
+        if let Ok(x) = x.parse::<usize>() {
+            if let Some(y) = args.next() {
+                if let Ok(y) = y.parse::<usize>() {
+                    let timer = time::Instant::now();
+                    funcs[x - 1][y - 1]();
+                    println!("Took {} seconds", timer.elapsed().as_secs_f32());
+                } else {
+                    println!("Must enter numbers!");
+                }
+            } else {
+                println!("Pass day and part as commandline parameters");
+            }
+        } else {
+            println!("Must enter numbers!");
+        }
+    } else {
+        println!("Pass day and part as commandline parameters");
+    }
+}
